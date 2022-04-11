2991031d4a45d10e2a0fb064a20562db3f7f1885
Mahamadou TOURE <touremohamed@@gmail.com>
2022-04-12 00:28:57 +0200

    - add mtls gateway config

---
+++ k8s/cert.sh
@@ -0,0 +1,32 @@
+#export DOMAIN_NAME=larbo.com
+
+export DOMAIN_NAME=localhost
+# As a first step, you are going to create the root certificate ($DOMAIN_NAME.crt) and the private key used for signing the certificate ($DOMAIN_NAME.key):
+openssl req -x509 -sha256 -nodes -days 365 -newkey rsa:2048 -subj '/O=localhost Inc./CN=localhost' -keyout root.private.$DOMAIN_NAME.key -out root.certificate.$DOMAIN_NAME.crt
+
+
+# Next, you need to create the private key:
+# create private key (employee.$DOMAIN_NAME.key) and certificate request or   certificate signing requests (CSR) (employee.$DOMAIN_NAME.csr ) use to create certificate
+openssl req -out $DOMAIN_NAME.csr -newkey rsa:2048 -nodes -keyout $DOMAIN_NAME.key -subj "/CN=$DOMAIN_NAME/O=api employee management from $DOMAIN_NAME"
+
+# And the certificate:
+openssl x509 -req -days 365 -CA root.certificate.$DOMAIN_NAME.crt -CAkey root.private.$DOMAIN_NAME.key -set_serial 0 -in $DOMAIN_NAME.csr -out $DOMAIN_NAME.crt
+
+kubectl create secret tls istio-ingressgateway-certs -n istio-system --key $DOMAIN_NAME.key --cert $DOMAIN_NAME.crt
+
+curl -v --cacert root.certificate.$DOMAIN_NAME.crt https://localhost:/app/employees
+
+
+######################################
+#Getting Chrome to accept self-signed localhost certificate
+
+#For localhost only
+#Simply paste this in your chrome:
+#
+#chrome://flags/#allow-insecure-localhost
+#You should see highlighted text saying: Allow invalid certificates for resources loaded from localhost
+#
+#Click Enable.
+
+openssl x509 -in $DOMAIN_NAME.crt -out $DOMAIN_NAME.crt.pem
+openssl x509 -in root.certificate.$DOMAIN_NAME.crt -out root.$DOMAIN_NAME.crt.pem
